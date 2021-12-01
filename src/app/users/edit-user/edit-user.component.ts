037ee68a9886aeaf93757f188bdc3f488b2dc773
Ashraf <ashrafunnisa931@gmail.com>
2021-12-01 23:42:14 +1100

    Implemented edit user component
    Added basic validations to add user componet and edit user component

--- src/app/users/edit-user/edit-user.component.ts
+++ src/app/users/edit-user/edit-user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-edit-user',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+  id: string = "";
+  userDetails: any;
+  editUserForm: FormGroup = new FormGroup({});
+  dataLoaded: boolean = false;
+  constructor(private activatedRoue: ActivatedRoute, private _userService: UserService, private formBuilder: FormBuilder, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
-  }
+    this.dataLoaded = false;
+    this.activatedRoue.params.subscribe(data => {
+      this.id = data["id"];
+    });
+    if(this.id !== '') {
+      //view user details
+      this._userService.viewUser(this.id)
+      .toPromise()
+      .then(data => {
+        this.userDetails = data;
+        Object.assign(this.userDetails, data);
 
+        //Build the edit form
+        this.editUserForm = this.formBuilder.group({
+          'name': new FormControl(this.userDetails.name, [Validators.required, Validators.minLength(3)]),
+          'email': new FormControl(this.userDetails.email, [Validators.required, Validators.email]),
+          'phone': new FormControl(this.userDetails.phone,[Validators.required, Validators.maxLength(10)])
+        })
+        this.dataLoaded = true;
+      })
+      .catch (err => {
+        console.log(err);
+      })
+    }
+  }
+  updateUser(){
+    console.log(this.editUserForm.value);
+    this._userService.updateUser(this.id, this.editUserForm.value).subscribe(data => {
+      this._snackBar.open("User updated successfully");
+    }, err => {
+      this._snackBar.open("Unable to update user:" + err);
+    })
+  }
 }
